eb327436f3ed25ef07092a39a6b0f252e35a09d8
jameijsden <jameijsden@gmail.com>
2015-11-06 12:13:19 +0100

    Added bInsertSort

---
+++ src/laborations/Laboration1.java
@@ -0,0 +1,73 @@
+package laborations;
+
+import java.util.Arrays;
+
+public class Laboration1 {
+	int[] seq;
+	public Laboration1(int[] seq){
+		this.seq = seq;
+	}
+	
+	public static void main(String[] args) {
+		int[] s = {1,24,7,2,8,6,2,1,7,80,6,3,2,6}; 
+		Laboration1 lab = new Laboration1(s); 
+		lab.bInsertionSort();
+	}
+	
+	
+	public void mMergeSort(){
+		//Hej hej tomte
+	}
+
+	int BinarySearch (int a[], int low, int high, int key)
+	{
+	    int mid;
+
+	    if (low == high)
+	        return low;
+
+	    mid = low + ((high - low) / 2);
+
+	    if (key > a[mid])
+	        return BinarySearch (a, mid + 1, high, key);
+	    else if (key < a[mid])
+	        return BinarySearch (a, low, mid, key);
+
+	    return mid;
+	}
+	
+	void bInsertionSort (){
+	    int ins, i, j;
+	    int tmp;
+
+	    for (i = 1; i < this.seq.length; i++) {
+	        ins = BinarySearch (this.seq, 0, i, this.seq[i]);
+	        tmp = this.seq[i];
+	        for (j = i - 1; j >= ins; j--)
+	            this.seq[j + 1] = this.seq[j];
+	        this.seq[ins] = tmp;
+	    }
+	    System.out.println(Arrays.toString(this.seq));
+	}
+	
+	public void InsertionSort(){		
+		int[] s = this.seq; 
+		System.out.println("Sorting: " + Arrays.toString(s));
+		for(int i = 1; i < s.length; i++){
+			int key = s[i];
+			for(int j = i-1; j >= 0; j--){
+				if(key < s[j]){
+					//System.out.println(key + " < " + s[j]);
+					s[j+1] = s[j];
+				}else {
+					//System.out.println(key + " >= " + s[j]);
+					s[j+1] = key; 
+					break;
+				}
+			}		
+			System.out.println("Sorting: " + Arrays.toString(s));
+		}
+		
+				
+	}
+}
