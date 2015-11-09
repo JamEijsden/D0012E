032ee338a750ad0de87cc38e13f0e5d1704af780
jameijsden <jameijsden@gmail.com>
2015-11-09 17:56:59 +0100

    working mergesort using bInsertionSort

--- src/laborations/Laboration1.java
+++ src/laborations/Laboration1.java
@@ -1,5 +1,7 @@
 package laborations;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Arrays;
 
 public class Laboration1 {
@@ -11,14 +13,21 @@ public class Laboration1 {
 
 	public static void main(String[] args) {
 		Laboration1 lab = new Laboration1();
-		lab.array = lab.generateRandomList(10);
-		System.out.println("START: "+Arrays.toString(lab.array));
+		lab.array = lab.generateRandomList(1000000);
+		//System.out.println("Start: "+Arrays.toString(lab.array));
 		// lab.mMergeSort1(s, 0, s.length);
-		lab.doMergeSort(5);
+		lab.doMergeSort(100);
+		
+		
 	}
 
 	public void doMergeSort(int k) {
+		NumberFormat formatter = new DecimalFormat("#0.00000");
+		long startTime = System.currentTimeMillis();
 		mMergeSort1(k, 0, this.array.length-1);
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.print("Execution time is " + formatter.format((totalTime) / 1000d) + " seconds");
 	}
 
 	private void mMergeSort1(int k, int start, int end) {
@@ -37,6 +46,7 @@ public class Laboration1 {
 	}
 
 	private void merge(int lowerIndex, int middle, int higherIndex) {
+		//System.out.println("Init merge: "+Arrays.toString(array));
 		int[] tempMergArr = new int[array.length];
 		for (int i = lowerIndex; i <= higherIndex; i++) {
 			tempMergArr[i] = array[i];
@@ -53,17 +63,14 @@ public class Laboration1 {
 				j++;
 			}
 			k++;
-			System.out.println("SOOORTING: "+Arrays.toString(array));
 		}
 		while (i <= middle) {
-			System.out.println("ENDING: "+Arrays.toString(array));
 			array[k] = tempMergArr[i];
 			k++;
 			i++;
-		
 		}
 		
-		System.out.println("Merged: " + lowerIndex + " " + middle + " "+ higherIndex +" "+ "\nResult: "+Arrays.toString(array));
+		//System.out.println("Merged: " + lowerIndex + " <-> " + higherIndex +" "+ "\nResult: "+Arrays.toString(array));
 	}
 
 	private void mMergeSort2() {
@@ -91,7 +98,7 @@ public class Laboration1 {
 		int ins, i, j;
 		int tmp;
 
-		for (i = start + 1; i < end; i++) {
+		for (i = start + 1; i <= end; i++) {
 			ins = BinarySearch(list, start, i, list[i]);
 			tmp = list[i];
 			for (j = i - 1; j >= ins; j--)
