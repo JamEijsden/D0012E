b8faafbd2759bcd4db516674fb1228015ee250a8
jameijsden <jameijsden@gmail.com>
2015-11-27 10:51:41 +0100

    NODE ADDED AGAIN

--- src/laboration2/Laboration2.java
+++ src/laboration2/Laboration2.java
@@ -2,8 +2,13 @@ package laboration2;
 
 public class Laboration2 {
 	
-	public boolean linearProbing(int x, int[] ht){
-		
+	public boolean linearProbing(int x, Node linkedN){
+		while(x%){
+			
+		}
+		if(){
+			
+		}
 		return false;
 		
 	}
