01f79083bbcebcd8b06358f1b40de30414838070
jameijsden <jameijsden@gmail.com>
2015-11-27 11:20:25 +0100

    linearProbing done

--- src/laboration2/Laboration2.java
+++ src/laboration2/Laboration2.java
@@ -1,16 +1,19 @@
 package laboration2;
 
 public class Laboration2 {
-	
-	public boolean linearProbing(int x, Node linkedN){
-		while(x%){
-			
+
+	public int linearProbing(int hx, int[] hashArray) {
+		int probe = hx % hashArray.length;
+		int ld = 0,lu = 0;
+		int probesDone = 0;
+		while(true) {
+			if (hashArray[probe] == 0){ 
+				System.out.println("Number of probes: " + probesDone);
+				return probe; 
+			}
+			if(ld <= lu) probe++; else probe--;
+			probesDone++;
 		}
-		if(){
-			
-		}
-		return false;
-		
 	}
-	
+
 }
