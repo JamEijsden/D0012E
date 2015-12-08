ede54a512a67e5c9a547b55f2349344f360c7595
jameijsden <jameijsden@gmail.com>
2015-12-08 10:29:47 +0100

    Working graph add

--- src/laboration3/Graph.java
+++ src/laboration3/Graph.java
@@ -3,6 +3,7 @@ package laboration3;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
 public class Graph {
 	int size;
@@ -55,28 +56,59 @@ public class Graph {
 	public int V(){
 		return graph.size();
 	}
-	public void addVertex(String id, String dest, int weight){
+	public void addEdge(String id, String dest, int weight){
 		Edge e = new Edge(dest, weight);
 		Edge dE = new Edge(id, weight);
-		if(this.graph.get(id).isEmpty()) {
+		if(!this.graph.containsKey(id)) {
 			ArrayList<Edge> newConn = new ArrayList<Edge>();
 			newConn.add(e);
 			this.graph.put(id, newConn);
 		} else
 			this.graph.get(id).add(e);
-		if(this.graph.get(dest).isEmpty()){
+		
+		if(!this.graph.containsKey(dest)){
 			ArrayList<Edge> newConn = new ArrayList<Edge>();
 			newConn.add(dE);
-			this.graph.put(id, newConn);
+			this.graph.put(dest, newConn);
 		}else
-			this.graph.get(dest).add(dE);
-			
-	}
-	public Collection<ArrayList<Graph.Edge>> Vertices (){
-		return this.graph.values();
+			this.graph.get(dest).add(dE);		
 	}
 	
-	public ArrayList<Edge> adjacentTo(String id){
-		return this.graph.get(id);
+	public Set<String> Vertices (){
+		return this.graph.keySet();
 	}
+	
+	public String[] adjacentTo(String id){
+		String[] neighbours = new String[this.graph.get(id).size()];
+		for(int i = 0; i < this.graph.get(id).size(); i++){
+			neighbours[i] = (this.graph.get(id)).get(i).getDest();	
+		}
+		return neighbours;
+	}
+	
+	public static void main(String[] args) {
+        Graph G = new Graph(10);
+        G.addEdge("A", "B", 5);
+        G.addEdge("A", "C", 2);
+        G.addEdge("C", "D", 7);
+        G.addEdge("D", "E", 8);
+        G.addEdge("B", "E", 3);
+        G.addEdge("C", "B", 12);
+        G.addEdge("D", "G", 1);
+        G.addEdge("E", "G", 5);
+        //G.addVertex("H");
+
+        // print out graph
+        System.out.println("Graph");
+
+        // print out graph again by iterating over vertices and edges
+        for (String v : G.Vertices()) {
+        	System.out.print(v + ": ");
+            for (String w : G.adjacentTo(v)) {
+            	System.out.print(w + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
