011445f9f21ff50260a4ba444c49fc6821ac0f25
jameijsden <jameijsden@gmail.com>
2015-12-11 09:22:12 +0100

    new type Node in heap

--- src/laboration3/DAryHeap.java
+++ src/laboration3/DAryHeap.java
@@ -12,43 +12,58 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 /** Class D-ary Heap **/
-class DAryHeap    
-{
-	private int[] data;
+class DAryHeap {
+	private Node[] data;
 	private int heapSize;
 	private int d;
-    public DAryHeap(int size, int d) {
-          data = new int[size];
-          heapSize = 0;
-          this.d = d;
-    }
 
-    public int getMinimum() {
-          if (isEmpty())
-                throw new HeapException("Heap is empty");
-          else
-                return data[0];
-    }
+	public DAryHeap(int size, int d) {
+		data = new Node[size];
+		heapSize = 0;
+		this.d = d;
+	}
 
-    public boolean isEmpty() {
-          return (heapSize == 0);
-    }
+	public class Node {
+		int dist;
+		String id;
 
-    private int getLeftChildIndex(int nodeIndex) {
-          return 2 * nodeIndex + 1;
-    }
+		public Node(String id, int dist) {
+			this.dist = dist;
+			this.id = id;
 
-    private int getRightChildIndex(int nodeIndex) {
-          return 2 * nodeIndex + 2;
-    }
+		}
 
-    private int getParentIndex(int nodeIndex) {
-          return (nodeIndex - 1) / 2;
-    }
+		public void updateKey() {
 
-    public class HeapException extends RuntimeException {
-          public HeapException(String message) {
-                super(message);
-          }
-    }     
+		}
+
+		public Node extractMin() {
+			if (isEmpty())
+				throw new HeapException("Heap is empty");
+			else
+				return data[0];
+		}
+
+		public boolean isEmpty() {
+			return (heapSize == 0);
+		}
+
+		private int getLeftChildIndex(int nodeIndex) {
+			return 2 * nodeIndex + 1;
+		}
+
+		private int getRightChildIndex(int nodeIndex) {
+			return 2 * nodeIndex + 2;
+		}
+
+		private int getParentIndex(int nodeIndex) {
+			return (nodeIndex - 1) / 2;
+		}
+
+		public class HeapException extends RuntimeException {
+			public HeapException(String message) {
+				super(message);
+			}
+		}
+	}
 }
